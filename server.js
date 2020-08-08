ff011b069a6f41ca698b2f6ec919e1e8ba7643c5
Nathan Ricciardi <nathan.ricciardi@gmail.com>
2020-08-08 11:10:04 -0700

    Updates made per request

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const MOVIE_DATA = require('./movies-data-small.json')
 const app = express()
+
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
 
 app.get('/movies', function handleGetMovies(req, res) {
   let response = MOVIE_DATA;
